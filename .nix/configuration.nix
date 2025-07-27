f9b428bb7129189133a12d3a27a1d2c53f48f58d
abdelmounim <abdelmounimbaouia@gmail.com>
2025-07-27 08:43:48 +0100

    nix: refactor: rename obsolete parameters

--- .nix/configuration.nix
+++ .nix/configuration.nix
@@ -75,7 +75,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -132,7 +132,7 @@
 
   hardware = {
     # Opengl
-    opengl.enable = true;
+    graphics.enable = true;
 
     # Most wayland compositors need this
     nvidia.modesetting.enable = true;
