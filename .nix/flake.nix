69c228c832197fc564bf28333be1291853654aa4
abdelmounim <abdelmounimbaouia@gmail.com>
2026-01-13 12:26:14 +0100

    refactor(nix): update flake with new modular structure and minimal configs

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -22,55 +22,131 @@
       ...
     }@inputs:
     let
-      lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
     in
     {
-      nixosConfigurations = {
-        workstation = lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            ./dev-tools.nix
-            ./docker.nix
-            # ./kubernetes.nix
-            ./games.nix
-            ./hyprland.nix
-            ./productivity.nix
-            ./tmux-plugins.nix
-            ./tools.nix
-            # ./uni-tools.nix
-            ./vsftpd.nix
-            ./vms.nix
-            ./nerdfonts.nix
-            ./xfce-i3.nix
-            ./work-tools.nix
-            ./ecom-tools.nix
-            # ./network.nix
-            # ./work-account.nix
-            { environment.systemPackages = with pkgs; [ hyprpanel ]; }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.users.ab = import ./home/home.nix;
-              home-manager.users.work = import ./home/work.nix;
-              home-manager.users.smeetz = import ./home/smeetz.nix;
-
-              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-            }
-          ];
-        };
-      };
-      # homeConfigurations = {
-      #   ab = home-manager.lib.homeManagerConfiguration {
-      #     inherit pkgs;
-      #     modules = [ ./home/home.nix ];
-      #   };
-      # };
-    };
+            nixosConfigurations = {
+              workstation = nixpkgs.lib.nixosSystem {
+                inherit system;
+                specialArgs = { inherit inputs; };
+                modules = [
+                  ./configuration.nix
+                  
+                  # Desktop Environment & Window Managers
+                  ./hyprland.nix
+                  ./xfce-i3.nix
+                  
+                  # Tools & Applications
+                  ./dev-tools.nix
+                  ./docker.nix
+                  ./vms.nix
+                  ./vsftpd.nix
+                  ./games.nix
+                  ./productivity.nix
+                  ./tmux-plugins.nix
+                  ./tools.nix
+                  ./nerdfonts.nix
+                  ./work-tools.nix
+                  ./ecom-tools.nix
+      
+                  # Home Manager for workstation
+                  home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.backupFileExtension = "backup";
+                    home-manager.extraSpecialArgs = { inherit inputs; };
+                    
+                    home-manager.users.ab = import ./home/home.nix;
+                    home-manager.users.work = import ./home/work.nix;
+                    home-manager.users.smeetz = import ./home/smeetz.nix;
+                  }
+                ];
+              };
+              
+                      minimal-cli = nixpkgs.lib.nixosSystem {
+              
+                        inherit system;
+              
+                        specialArgs = { inherit inputs; };
+              
+                        modules = [
+              
+                          ./minimal-cli.nix
+              
+                          ./tmux-plugins.nix
+              
+                          
+              
+                          # Home Manager for minimal
+              
+                          home-manager.nixosModules.home-manager
+              
+                          {
+              
+                            home-manager.useGlobalPkgs = true;
+              
+                            home-manager.useUserPackages = true;
+              
+                            home-manager.backupFileExtension = "backup";
+              
+                            home-manager.extraSpecialArgs = { inherit inputs; };
+              
+                            
+              
+                            # Only import the 'ab' user
+              
+                            home-manager.users.ab = import ./home/home.nix;
+              
+                          }
+              
+                        ];
+              
+                      };
+              
+              
+              
+                      minimal-gui = nixpkgs.lib.nixosSystem {
+              
+                        inherit system;
+              
+                        specialArgs = { inherit inputs; };
+              
+                        modules = [
+              
+                          ./minimal-gui.nix
+              
+                          ./tmux-plugins.nix
+              
+                          ./nerdfonts.nix
+              
+                          
+              
+                          # Home Manager for minimal
+              
+                          home-manager.nixosModules.home-manager
+              
+                          {
+              
+                            home-manager.useGlobalPkgs = true;
+              
+                            home-manager.useUserPackages = true;
+              
+                            home-manager.backupFileExtension = "backup";
+              
+                            home-manager.extraSpecialArgs = { inherit inputs; };
+              
+                            
+              
+                            # Only import the 'ab' user
+              
+                            home-manager.users.ab = import ./home/home.nix;
+              
+                          }
+              
+                        ];
+              
+                      };
+              
+                    };    };
 }
