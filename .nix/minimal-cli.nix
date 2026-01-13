319effe0372759d84c7568415a1c003fe32eb2c0
abdelmounim <abdelmounimbaouia@gmail.com>
2026-01-13 12:26:07 +0100

    feat(nix): add minimal-cli configuration

---
+++ .nix/minimal-cli.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./core.nix
+    # This should point to the hardware configuration for your new machine.
+    # Generate it with `nixos-generate-config` on the new machine and copy it here.
+    ./minimal-hardware.nix 
+  ];
+
+  # ==========================================
+  # Hostname
+  # ==========================================
+  networking.hostName = "minimal-cli"; 
+
+  # ==========================================
+  # User for Minimal Machine
+  # ==========================================
+  users.users.ab = {
+    isNormalUser = true;
+    description = "Abdelmounim Baouia";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+  };
+
+  # ==========================================
+  # Packages for Minimal Machine
+  # ==========================================
+  environment.systemPackages = with pkgs; [
+    # Add any extra packages for the minimal machine here
+    # e.g. tmux
+  ];
+}
