3ba753ad80e9f98d07ca7546f8940655c8d37818
abdelmounim <abdelmounimbaouia@gmail.com>
2026-01-13 12:26:05 +0100

    refactor(nix): remove redundant settings from hyprland.nix

--- .nix/hyprland.nix
+++ .nix/hyprland.nix
@@ -6,15 +6,6 @@
     enable = true;
     xwayland.enable = true;
   };
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
-
-  hardware = {
-    graphics.enable = true;
-    nvidia.modesetting.enable = true;
-  };
 
   xdg = {
     portal = {
